0881fb3a999ea9b3529a29327af6733561d439df
itttgg <spdd5724@gmail.com>
2022-12-15 09:23:04 +0300

    Edit title

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import code from '../styles/code.css'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className={styles.title}>Welcome to <strong className={styles.code}>Hello, world</strong></h1>
+      <h1 className={styles.title}>Welcome to <a className={code.string}>'Hello, world'</a></h1>
 
       <main>
         <p className={styles.description}>
