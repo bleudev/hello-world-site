5c0a6bbb2176d59b32f75163e0060558c7182176
itttgg <spdd5724@gmail.com>
2022-12-15 10:27:04 +0300

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Text from 'react'
+import { Text } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
