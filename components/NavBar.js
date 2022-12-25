dbd50709b002d41b76caa648bd9bf9f46ff6bfc0
itttgg <spdd5724@gmail.com>
2022-12-25 12:05:14 +0300

    Bug fix again

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,7 +2,7 @@ import { Component } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 
-import styles from '../styles/Navbar.Component.css'
+import styles from './NavBar_styles.css'
 
 export default class NavBar extends Component {
     render() {
