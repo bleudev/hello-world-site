3f967d2db4b2d24e67036d69b7fae8046c04bc8e
itttgg <spdd5724@gmail.com>
2022-12-25 11:53:56 +0300

    bug fix

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,19 +1,22 @@
 import { Component } from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
+
+import styles from '../styles/Navbar.Component.css'
 
 export default class NavBar extends Component {
     render() {
         return (
-        <div className='navbar'>
-            <div className='navlogo'>
+        <div className={styles.navbar}>
+            <div className={styles.navlogo}>
                 Hello, world
             </div>
-            <div className='navlink'>
+            <div className={styles.navlink}>
                 <Link href='/learn'>
                     <a>Learn</a>
                 </Link>
             </div>
-            <div className='navlink'>
+            <div className={styles.navlink}>
                 <Link href='/test'>
                     <a>Test</a>
                 </Link>
