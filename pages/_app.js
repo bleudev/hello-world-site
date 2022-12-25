6d21ccaeaa519a32438a827337382aa4d0b301c0
itttgg <spdd5724@gmail.com>
2022-12-25 11:39:39 +0300

    bug fix №2

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import '../styles/learn-page.css'
-import '../styles/Navbar.Component.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
